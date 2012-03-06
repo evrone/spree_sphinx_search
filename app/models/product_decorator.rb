a6ec69762ce7ebcdf8513886851142988691aefa
Aleksey <aleksey.dem@gmail.com>
2012-03-07 01:36:36 +0400

    Properties indexing

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,6 +1,7 @@
 Spree::Product.class_eval do
-  class_attribute :indexed_options
+  class_attribute :indexed_options, :indexed_properties
   self.indexed_options = []
+  self.indexed_properties = []
 
   def self.sphinx_search_options &rules
     Spree::Search::ThinkingSphinx.send :define_method, :custom_options, rules
@@ -22,6 +23,16 @@ Spree::Product.class_eval do
       sql.gsub("\n", ' ').gsub('  ', '')
     end
 
+    property_sql = lambda do |property_name|
+      sql = <<-eos
+          (SELECT spp.value
+          FROM spree_product_properties AS spp
+          INNER JOIN spree_properties AS sp ON sp.id = spp.property_id
+          WHERE sp.name = '#{property_name}' AND spp.product_id = spree_products.id)
+      eos
+      sql.gsub("\n", ' ').gsub('  ', '')
+    end
+
     indexes :name
     indexes :description
     indexes :meta_description
@@ -38,6 +49,9 @@ Spree::Product.class_eval do
     group_by "spree_products.deleted_at"
     group_by :available_on
     has is_active_sql, :as => :is_active, :type => :boolean
+    source.model.indexed_properties.each do |prop|
+      has property_sql.call(prop[:name].to_s), :as => :"#{prop[:name]}_property", :type => prop[:type]
+    end
     source.model.indexed_options.each do |opt|
       has option_sql.call(opt.to_s), :as => :"#{opt}_option", :source => :ranged_query, :type => :multi, :facet => true
     end
