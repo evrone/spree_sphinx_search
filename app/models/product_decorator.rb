1943b328abcebb18b06b9eed7fe283cd1ff27f36
Aleksey <aleksey.dem@gmail.com>
2012-03-09 22:19:13 +0400

    Sorting by product name

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -33,7 +33,7 @@ Spree::Product.class_eval do
       sql.gsub("\n", ' ').gsub('  ', '')
     end
 
-    indexes :name
+    indexes :name, :sortable => true
     indexes :description
     indexes :meta_description
     indexes :meta_keywords
