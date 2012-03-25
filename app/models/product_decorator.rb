8777c6e80c10308bb0b8cd41a8d735b8c7021ec7
Aleksey <aleksey.dem@gmail.com>
2012-03-25 17:56:11 +0400

    Changed description for indexed_attributes

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,10 +1,12 @@
 Spree::Product.class_eval do
-  class_attribute :indexed_options, :indexed_properties, :indexed_attributes
+  class_attribute :indexed_options, :indexed_properties
   self.indexed_options = []
   # [{:name => :age_from, :type => :integer}]
   self.indexed_properties = []
-  # [{:field => :created_at, :options => {:as => :recency}}]
-  self.indexed_attributes = []
+  # Method should return array of hashes like [{:field => :created_at, :options => {:as => :recency}}]
+  def self.indexed_attributes
+    []
+  end
 
   def self.sphinx_search_options &rules
     Spree::Search::ThinkingSphinx.send :define_method, :custom_options, rules
